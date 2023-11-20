c61dbeb553ca630e5e68639e1a7e3da439d977b5
Emanuel3k <emanuelsantos_677@hotmail.com>
2023-11-20 16:24:47 -0300

    Feat: Addition of a block that initializes the application with a list to be worked on in other methods

--- src/main/kotlin/br/com/emanuel3k/service/TopicService.kt
+++ src/main/kotlin/br/com/emanuel3k/service/TopicService.kt
@@ -6,9 +6,12 @@ import br.com.emanuel3k.model.User
 import jakarta.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
-class TopicService {
-    fun list(): List<Topic> {
-        val topic = Topic(
+class TopicService(
+    private var topics: List<Topic> = ArrayList(),
+) {
+
+    init {
+        val t1 = Topic(
             id = 1,
             title = "Duvida Kotlin",
             message = "Variaveis no Kotlin",
@@ -23,7 +26,41 @@ class TopicService {
                 email = "ana@email.com"
             )
         )
+        val t2 = Topic(
+            id = 2,
+            title = "Duvida Kotlin",
+            message = "Variaveis no Kotlin",
+            course = Course(
+                id = 1,
+                name = "Kotlin",
+                category = "Programacao"
+            ),
+            author = User(
+                id = 1,
+                name = "Ana da Silva",
+                email = "ana@email.com"
+            )
+        )
+        val t3 = Topic(
+            id = 3,
+            title = "Duvida Kotlin",
+            message = "Variaveis no Kotlin",
+            course = Course(
+                id = 1,
+                name = "Kotlin",
+                category = "Programacao"
+            ),
+            author = User(
+                id = 1,
+                name = "Ana da Silva",
+                email = "ana@email.com"
+            )
+        )
 
-        return arrayListOf(topic, topic)
+        this.topics = arrayListOf(t1, t2, t3)
+    }
+
+    fun list(): List<Topic> {
+        return topics
     }
 }
