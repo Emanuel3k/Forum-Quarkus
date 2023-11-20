21ad0e6dfcc1a0d8beac264487c7b02a4e7b87b5
Emanuel3k <emanuelsantos_677@hotmail.com>
2023-11-20 15:57:53 -0300

    Feat: To continue following the clean architecture, I sent all logic to the service layer and injected it into the controller

--- src/main/kotlin/br/com/emanuel3k/controller/TopicController.kt
+++ src/main/kotlin/br/com/emanuel3k/controller/TopicController.kt
@@ -1,32 +1,19 @@
 package br.com.emanuel3k.controller
 
-import br.com.emanuel3k.model.Course
 import br.com.emanuel3k.model.Topic
-import br.com.emanuel3k.model.User
+import br.com.emanuel3k.service.TopicService
+import jakarta.inject.Inject
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 
 @Path("/topics")
-class TopicController {
+class TopicController(
+    @Inject
+    private val service: TopicService,
+) {
 
     @GET
     fun list(): List<Topic> {
-        val topic = Topic(
-            id = 1,
-            title = "Duvida Kotlin",
-            message = "Variaveis no Kotlin",
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "Programacao"
-            ),
-            author = User(
-                id = 1,
-                name = "Ana da Silva",
-                email = "ana@email.com"
-            )
-        )
-
-        return  arrayListOf(topic, topic)
+        return service.list()
     }
 }
