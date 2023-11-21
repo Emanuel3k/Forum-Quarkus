e83e4d5691b12366572c9157107cb036726d9fd2
Emanuel3k <emanuelsantos_677@hotmail.com>
2023-11-21 16:34:57 -0300

    Fix: I changed the id from VAL to VAR

--- src/main/kotlin/br/com/emanuel3k/model/Topic.kt
+++ src/main/kotlin/br/com/emanuel3k/model/Topic.kt
@@ -4,7 +4,7 @@ import br.com.emanuel3k.model.enums.StatusTopic
 import java.time.LocalDateTime
 
 data class Topic(
-    val id: Long? = null,
+    var id: Long? = null,
     val title: String,
     val message: String,
     val creationDate: LocalDateTime = LocalDateTime.now(),
