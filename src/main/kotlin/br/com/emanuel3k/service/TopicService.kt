0c7ca9bfb71920a1f248a928bce7f4379955c99f
Emanuel3k <emanuelsantos_677@hotmail.com>
2023-11-21 12:22:29 -0300

    Feat: Creation of the post com method and removal of the list that started with predefined values

--- src/main/kotlin/br/com/emanuel3k/service/TopicService.kt
+++ src/main/kotlin/br/com/emanuel3k/service/TopicService.kt
@@ -1,65 +1,12 @@
 package br.com.emanuel3k.service
 
-import br.com.emanuel3k.model.Course
 import br.com.emanuel3k.model.Topic
-import br.com.emanuel3k.model.User
 import jakarta.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class TopicService(
     private var topics: List<Topic> = ArrayList(),
 ) {
-
-    init {
-        val t1 = Topic(
-            id = 1,
-            title = "Duvida Kotlin",
-            message = "Variaveis no Kotlin",
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "Programacao"
-            ),
-            author = User(
-                id = 1,
-                name = "Ana da Silva",
-                email = "ana@email.com"
-            )
-        )
-        val t2 = Topic(
-            id = 2,
-            title = "Duvida Kotlin",
-            message = "Variaveis no Kotlin",
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "Programacao"
-            ),
-            author = User(
-                id = 1,
-                name = "Ana da Silva",
-                email = "ana@email.com"
-            )
-        )
-        val t3 = Topic(
-            id = 3,
-            title = "Duvida Kotlin",
-            message = "Variaveis no Kotlin",
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "Programacao"
-            ),
-            author = User(
-                id = 1,
-                name = "Ana da Silva",
-                email = "ana@email.com"
-            )
-        )
-
-        this.topics = arrayListOf(t1, t2, t3)
-    }
-
     fun list(): List<Topic> {
         return topics
     }
@@ -68,5 +15,9 @@ class TopicService(
         return topics.stream().filter { t ->
             t.id == id
         }.findFirst().get()
+    }
+
+    fun register(topic: Topic) {
+        topics.plus(topic)
     }
 }
