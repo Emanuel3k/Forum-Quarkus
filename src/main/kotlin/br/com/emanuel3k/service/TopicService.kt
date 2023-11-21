1d242e2c37797109567daaaffeb16a9c39e40328
Emanuel3k <emanuelsantos_677@hotmail.com>
2023-11-21 16:36:41 -0300

    Feat: I transfer the class mapping logic to an extra layer using the clean architecture mapper pattern

--- src/main/kotlin/br/com/emanuel3k/service/TopicService.kt
+++ src/main/kotlin/br/com/emanuel3k/service/TopicService.kt
@@ -2,6 +2,8 @@ package br.com.emanuel3k.service
 
 import br.com.emanuel3k.dto.TopicForm
 import br.com.emanuel3k.dto.TopicView
+import br.com.emanuel3k.mapper.TopicFormMapper
+import br.com.emanuel3k.mapper.TopicViewMapper
 import br.com.emanuel3k.model.Topic
 import jakarta.enterprise.context.ApplicationScoped
 import java.util.stream.Collectors
@@ -9,42 +11,26 @@ import java.util.stream.Collectors
 @ApplicationScoped
 class TopicService(
     private var topics: List<Topic> = ArrayList(),
-    private val courseService: CourseService = CourseService(),
-    private val userService: UserService = UserService(),
+    private val topicViewMapper: TopicViewMapper = TopicViewMapper(),
+    private val topicFormMapper: TopicFormMapper = TopicFormMapper(),
 ) {
     fun list(): List<TopicView> {
         return topics.stream().map { t ->
-            TopicView(
-                id = t.id,
-                title = t.title,
-                message = t.title,
-                creationDate = t.creationDate,
-                status = t.status
-            )
+            topicViewMapper.map(t)
         }.collect(Collectors.toList())
     }
 
     fun getById(id: Long): TopicView {
-        return topics.stream().map { t ->
-            TopicView(
-                id = t.id,
-                title = t.title,
-                message = t.title,
-                creationDate = t.creationDate,
-                status = t.status
-            )
+        val topic = topics.stream().filter { t ->
+            t.id == id
         }.findFirst().get()
+
+        return topicViewMapper.map(topic)
     }
 
     fun register(dto: TopicForm) {
-        topics = topics.plus(
-            Topic(
-                id = topics.size.toLong() + 1,
-                title = dto.title,
-                message = dto.message,
-                course = courseService.getById(dto.idCourse),
-                author = userService.getById(dto.idAuthor)
-            )
-        )
+        val topic = topicFormMapper.map(dto)
+        topic.id = topics.size.toLong() + 1
+        topics = topics.plus(topic)
     }
 }
