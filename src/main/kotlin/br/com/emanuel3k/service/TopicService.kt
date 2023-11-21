12784b7e5c405950515c9aafb650cd31dd029aef
Emanuel3k <emanuelsantos_677@hotmail.com>
2023-11-21 14:15:23 -0300

    Feat: I abstracted the registration method return to a DTO and converted the DTO topic to topic before actually registering

--- src/main/kotlin/br/com/emanuel3k/service/TopicService.kt
+++ src/main/kotlin/br/com/emanuel3k/service/TopicService.kt
@@ -1,11 +1,14 @@
 package br.com.emanuel3k.service
 
+import br.com.emanuel3k.dto.NewTopicDTO
 import br.com.emanuel3k.model.Topic
 import jakarta.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class TopicService(
     private var topics: List<Topic> = ArrayList(),
+    private val courseService: CourseService = CourseService(),
+    private val userService: UserService = UserService(),
 ) {
     fun list(): List<Topic> {
         return topics
@@ -17,7 +20,15 @@ class TopicService(
         }.findFirst().get()
     }
 
-    fun register(topic: Topic) {
-        topics.plus(topic)
+    fun register(dto: NewTopicDTO) {
+        topics = topics.plus(
+            Topic(
+                id = topics.size.toLong() + 1,
+                title = dto.title,
+                message = dto.message,
+                course = courseService.getById(dto.idCourse),
+                author = userService.getById(dto.idAuthor)
+            )
+        )
     }
 }
