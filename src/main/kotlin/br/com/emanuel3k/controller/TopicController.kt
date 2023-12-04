c813c50d9be29dea7c3f02c6b5e55acb3c9cbf00
Emanuel3k <emanuelsantos_677@hotmail.com>
2023-12-04 17:55:24 -0300

    Feat: Response return and add try catch in methods

--- src/main/kotlin/br/com/emanuel3k/controller/TopicController.kt
+++ src/main/kotlin/br/com/emanuel3k/controller/TopicController.kt
@@ -7,37 +7,37 @@ import br.com.emanuel3k.service.TopicService
 import jakarta.inject.Inject
 import jakarta.validation.Valid
 import jakarta.ws.rs.*
+import jakarta.ws.rs.core.Response
 
 @Path("/topics")
-class TopicController(
-    @field:Inject
-    private val service: TopicService,
-) {
+class TopicController {
+    @Inject
+    private lateinit var service: TopicService
 
     @GET
-    fun list(): List<TopicView> {
+    fun list(): Response {
         return service.list()
     }
 
     @GET
     @Path("/{id}")
-    fun getById(@PathParam("id") id: Long): TopicView {
+    fun getById(@PathParam("id") id: Long): Response {
         return service.getById(id)
     }
 
     @POST
-    fun register(@Valid dto: TopicForm) {
-        service.register(dto)
+    fun register(@Valid dto: TopicForm): Response {
+        return service.register(dto)
     }
 
     @PUT
-    fun update(@Valid dto: TopicUpdate) {
-        service.update(dto)
+    fun update(@Valid dto: TopicUpdate): Response {
+        return service.update(dto)
     }
 
     @DELETE
     @Path("/{id}")
-    fun delete(@PathParam("id") id: Long) {
-        service.delete(id)
+    fun delete(@PathParam("id") id: Long): Response {
+        return service.delete(id)
     }
 }
--- src/main/kotlin/br/com/emanuel3k/service/TopicService.kt
+++ src/main/kotlin/br/com/emanuel3k/service/TopicService.kt
@@ -2,12 +2,13 @@ package br.com.emanuel3k.service
 
 import br.com.emanuel3k.dto.TopicForm
 import br.com.emanuel3k.dto.TopicUpdate
-import br.com.emanuel3k.dto.TopicView
 import br.com.emanuel3k.mapper.TopicFormMapper
 import br.com.emanuel3k.mapper.TopicViewMapper
 import br.com.emanuel3k.model.Topic
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.inject.Inject
+import jakarta.ws.rs.core.Response
+import java.net.URI
 import java.util.stream.Collectors
 
 @ApplicationScoped
@@ -16,52 +17,85 @@ class TopicService(
 ) {
     @Inject
     private lateinit var topicViewMapper: TopicViewMapper
+
     @Inject
     private lateinit var topicFormMapper: TopicFormMapper
 
-    fun list(): List<TopicView> {
-        return topics.stream().map { t ->
-            topicViewMapper.map(t)
-        }.collect(Collectors.toList())
+    fun list(): Response {
+        if (topics.isEmpty()) {
+            return Response.noContent().build()
+        }
+
+        return try {
+
+            Response.ok(topics.stream().map { t ->
+                topicViewMapper.map(t)
+            }.collect(Collectors.toList())).build()
+
+        } catch (e: Exception) {
+            Response.status(400, "ERROR: ${e.message}").build()
+        }
     }
 
-    fun getById(id: Long): TopicView {
-        val topic = topics.stream().filter { t ->
-            t.id == id
-        }.findFirst().get()
+    fun getById(id: Long): Response {
+        return try {
+            val topic = topics.stream().filter { t ->
+                t.id == id
+            }.findFirst().get()
 
-        return topicViewMapper.map(topic)
+            Response.ok(topicViewMapper.map(topic)).build()
+        } catch (e: Exception) {
+            Response.status(400, "ERROR: ${e.message}").build()
+        }
     }
 
-    fun register(dto: TopicForm) {
-        val topic = topicFormMapper.map(dto)
-        topic.id = topics.size.toLong() + 1
-        topics = topics.plus(topic)
+    fun register(dto: TopicForm): Response {
+        return try {
+            val topic = topicFormMapper.map(dto)
+            topic.id = topics.size.toLong() + 1
+            topics = topics.plus(topic)
+            Response.created(URI("/topic/${topic.id}")).build()
+
+        } catch (e: Exception) {
+            Response.status(400, "ERROR: ${e.message}").build()
+        }
     }
 
-    fun update(dto: TopicUpdate) {
-        val topic = topics.stream().filter { t ->
-            t.id == dto.id
-        }.findFirst().get()
+    fun update(dto: TopicUpdate): Response {
+        return try {
+            val topic = topics.stream().filter { t ->
+                t.id == dto.id
+            }.findFirst().get()
 
-        topics = topics.minus(topic).plus(
-            Topic(
-                id = dto.id,
-                message = dto.message,
-                title = dto.title,
-                author = topic.author,
-                creationDate = topic.creationDate,
-                status = topic.status,
-                course = topic.course,
+            topics = topics.minus(topic).plus(
+                Topic(
+                    id = dto.id,
+                    message = dto.message,
+                    title = dto.title,
+                    author = topic.author,
+                    creationDate = topic.creationDate,
+                    status = topic.status,
+                    course = topic.course,
+                )
             )
-        )
+
+            return Response.ok(topicViewMapper.map(topic)).build()
+        } catch (e: Exception) {
+            Response.status(400, "ERROR: ${e.message}").build()
+        }
     }
 
-    fun delete(id: Long) {
-        val topic = topics.stream().filter { t ->
-            t.id == id
-        }.findFirst().get()
+    fun delete(id: Long): Response {
+        return try {
+            val topic = topics.stream().filter { t ->
+                t.id == id
+            }.findFirst().get()
 
-        topics = topics.minus(topic)
+            topics = topics.minus(topic)
+            Response.ok("Deletado com sucesso!").build()
+        } catch (e: Exception) {
+            Response.status(400, "${e.message}").build()
+        }
+
     }
 }
